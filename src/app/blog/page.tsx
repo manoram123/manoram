49b87c1b412aa98a2d0a7df775655d5a9c40f59c
Manoram Baudel <baudelmanoram123@gmail.com>
2023-11-16 02:07:48 +0545

    any

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ type BlogProps = {
   blogs: Array<BlogType>;
 };
 
-const Blogs = ({ blogs }: BlogProps) => {
+const Blogs = ({ blogs }: any) => {
   return (
     <div className="">
       <div className="px-5 md:px-0 md:w-3/5 mx-auto">
