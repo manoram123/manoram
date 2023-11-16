8883cc48b1fe3abce19858cbf2bec75bf49c4c87
Manoram Baudel <baudelmanoram123@gmail.com>
2023-11-16 11:59:23 +0545

    changes

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -42,7 +42,7 @@ const Layout = async () => {
 
   const blogs = await fetchBlog(query);
 
-  return <Blogs blogs={blogs ? blogs.posts : []} />;
+  return <Blogs blogs={blogs ? blogs.posts : null} />;
 };
 
 export default Layout;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import BlogClient from "./BlogClient";
 
-const Blog = ({ blogs }: { blogs: any }) => {
-  //TODO - Fix type error for blogs
+const page = ({ blogs }: any) => {
+  // TODO - fix type error of blog prop
   return (
-    <div className="">
+    <div>
       <div className="px-5 md:px-0 md:w-3/5 mx-auto">
         <BlogClient blogs={blogs} />
       </div>
@@ -12,4 +12,4 @@ const Blog = ({ blogs }: { blogs: any }) => {
   );
 };
 
-export default Blog;
+export default page;
